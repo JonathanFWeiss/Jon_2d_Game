11b9bd42236fd2ce7f861af2d27ad90b51eb7f72
JonathanFWeiss <jonathan.weiss@gmail.com>
2026-04-22 00:15:33 -0400

    added crossfade on room transition

--- Assets/Scripts/RoomTransition.cs
+++ Assets/Scripts/RoomTransition.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class RoomTransition : MonoBehaviour
 {
+    public Animator transitionAnim;
     public string sceneToLoad;
-    public void changeSceneNow()
+    private float transitionTime = 1f; // Duration of the transition animation
+    public void ChangeSceneNow()
     {
         SceneManager.LoadScene(sceneToLoad);
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Enering new room");
-       changeSceneNow();
+        //changeSceneNow();
+        StartCoroutine(TransitionCoroutine());
+    }
+
+    IEnumerator TransitionCoroutine()
+    {
+        transitionAnim.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime); // Wait for the animation to finish
+        ChangeSceneNow();
     }
 }
